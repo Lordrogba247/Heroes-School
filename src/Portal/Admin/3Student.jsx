ec526596ca3969cb3b8bcba981fb7ad9424f55be
Lordrogba247 <adedapomichael55@gmail.com>
2026-07-22 05:53:56 +0100

    student updtaed

--- src/Portal/Admin/3Student.jsx
+++ src/Portal/Admin/3Student.jsx
@@ -5,6 +5,7 @@ import AddStudentModal from "./4Add";
 const BASE_URL = "https://heroesschool-management-backend.vercel.app";
 
 const classOptions = [
+    "Creche", "Nursery 1", "Nursery 2",
     "Primary 1", "Primary 2", "Primary 3", "Primary 4", "Primary 5",
     "JSS 1", "JSS 2", "JSS 3",
     "SSS 1", "SSS 2", "SSS 3",
